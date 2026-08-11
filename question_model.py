a00ba63af0977aefcfea2291acf773485f952cd9
Angelo Fores <foresaj@gmail.com>
2026-08-11 16:46:18 +0800

    Update question_model.py

--- question_model.py
+++ question_model.py
@@ -1,6 +1,6 @@
 class Question:
     """This class allows questions in the question bank to become an object, with attributes such as the question itself and
-     it's answer."""
+     it's answer. Allows for easy access to the question itself."""
     def __init__(self, text, answer):
         self.text = text
         self.answer = answer
