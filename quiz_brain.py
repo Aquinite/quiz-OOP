1e42c3562e994e912ac16e39cfd86df41b1da4d2
Angelo Fores <foresaj@gmail.com>
2026-08-11 16:47:11 +0800

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,31 +1,31 @@
+import html
+
+
 class QuizBrain:
     """This class is responsible for storing the question list of the quiz, displaying each question of the quiz,
      and calculating and displaying the score of the user.
-     Has initial attributes: question number, questin list, and score"""
+     Has initial attributes: question number, question list, current score, and the current question."""
     def __init__(self,q_list):
         self.question_number = 0
         self.q_list = q_list
         self.score = 0
+        self.current_question = None
 
-    def check_answer_and_score(self, user_answer, actual_answer):
-        """Given parameter user_answer (the user's inputted answer) and actual_answer (from the question list passed into this class)
-        , this function checks if the user answered correctly then updates the user's score based on the answer."""
+    def check_answer_and_score(self, user_answer):
+        """Returns a boolean True or False depending on if the actual answer matches the user answer."""
+        actual_answer = self.current_question.answer
         if user_answer.lower() == actual_answer.lower():
             self.score += 1
-            print("You got it!")
-        elif user_answer.lower() != actual_answer.lower():
-            print("That's wrong. 😑")
-        print(f"The correct answer is: {actual_answer}")
-        print(f"Your current score is: {self.score}/{self.question_number}")
+            return True
+        else:
+            return False
 
     def next_question(self):
         """When ran, this function allows the program to display the current question and allows the user to input their answer."""
-        new_question = self.q_list[self.question_number]
-        self.question_number += 1 # We need to do this in order to call the new question when this function is called upon again.
-        user_answer = input(f"Q.{self.question_number}:{new_question.text} (True/False?) ")
-        self.check_answer_and_score(user_answer, new_question.answer) # runs the check answer and score function in order to
-        #correctly update users score and give feedback to the user regarding their answer.
-        print("\n")
+        self.current_question = self.q_list[self.question_number]
+        self.question_number += 1
+        question_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}:{question_text} (True/False?) "
 
     def still_has_questions(self):
         """Returns a boolean False or True in order for the while loop inside the main file to continue running the function "next_question".
@@ -34,3 +34,10 @@ class QuizBrain:
             return False
         else:
             return True
+
+    def reset_quiz(self):
+        """Resets your score and allows the UI to display the first question again."""
+        self.score = 0
+        self.question_number = 0
+        self.current_question = None
+
