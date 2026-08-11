72501afeacef07a29143a7ded76b8e41e94d88ef
Angelo Fores <foresaj@gmail.com>
2026-08-11 16:45:19 +0800

    Update clean_file.py

--- clean_file.py
+++ clean_file.py
@@ -1,17 +1,13 @@
 import question_model
 import data
 import quiz_brain
+import ui
 
 question_bank = []
-
 
 for question in data.question_data:
     transferred_question = question_model.Question(question["question"], question["correct_answer"])
     question_bank.append(transferred_question)
 
 new_quiz = quiz_brain.QuizBrain(question_bank)
-
-while new_quiz.still_has_questions():
-    new_quiz.next_question()
-
-print("You've completed the quiz!", f"Your final score is {new_quiz.score}/{new_quiz.question_number}.")
+quiz_ui = ui.QuizUI(new_quiz)
