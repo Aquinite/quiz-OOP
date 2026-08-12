82a23a3c1c36baa7188c805ceff0c9c7882f2bc9
Angelo Fores <foresaj@gmail.com>
2026-08-13 02:08:22 +0800

    Update clean_file.py

--- clean_file.py
+++ clean_file.py
@@ -1,13 +1,8 @@
-import question_model
 import data
 import quiz_brain
 import ui
 
-question_bank = []
 
-for question in data.question_data:
-    transferred_question = question_model.Question(question["question"], question["correct_answer"])
-    question_bank.append(transferred_question)
-
+question_bank = data.new_q_bank()
 new_quiz = quiz_brain.QuizBrain(question_bank)
 quiz_ui = ui.QuizUI(new_quiz)
